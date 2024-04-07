5ddb3187b0e4c6eb23c272d550a21c4f20195a63
yencheng1028 <125673176+yencheng1028@users.noreply.github.com>
2024-04-07 11:57:15 +0800

    Add files via upload
    add cmake_template

---
+++ cmake_template/cmake/deps.cmake
@@ -0,0 +1,37 @@
+# Project Dependences Configuration
+
+# Backup and set build type to release
+if(NOT MSVC)
+    set(CMAKE_BUILD_TYPE_BAK ${CMAKE_BUILD_TYPE})
+    set(CMAKE_BUILD_TYPE Release)
+endif()
+
+# Include subdirectories
+include_directories(${DEPS_PATHS})
+
+# Find other dependences
+set(OpenCV_STATIC OFF CACHE BOOL "Using OpenCV static linking library")
+#find_package(OpenCV REQUIRED)
+find_package(OpenCV)
+include_directories(${OpenCV_INCLUDE_DIRS})
+
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+
+#find_package(Serialport REQUIRED)
+find_package(Serialport)
+include_directories(${SERIALPORT_INCLUDE_DIRS})
+
+#find_package(Fuzzylite REQUIRED)
+find_package(Fuzzylite)
+include_directories(${Fuzzylite_INCLUDE_DIRS})
+
+# Add subdirectory
+foreach(DEPS_PATH ${DEPS_PATHS})
+    add_subdirectory(${DEPS_PATH})
+endforeach()
+
+# Restore origin build type
+if(NOT MSVC)
+    set(CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE_BAK})
+endif()
+
---
+++ cmake_template/util/util2/main.cpp
@@ -0,0 +1,10 @@
+#include <demo.h>
+#include <deps2.h>
+
+int main()
+{
+    demo_cpp();
+    deps2();
+
+    return 0;
+}
